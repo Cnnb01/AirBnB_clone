9d3c80a0bf8e23abfff92f0607fad1e602b24da8
Cnnb01 <nyamambanyaguch@gmail.com>
2024-02-08 18:22:10 +0300

    models

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,10 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        # self.kwargs = kwargs
     
     def __str__(self):
         return f"{self.__class__.__name__} {self.id}"
@@ -17,9 +16,6 @@ class BaseModel:
         return self.updated_at 
     
     def to_dict(self):
-        # if kwargs:
-        #     for k in kwargs.iteritems():
-
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         return self.__dict__ 
