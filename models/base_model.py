c23fe66acf389ba741d234bb85dd09a3e92ffd6a
Cnnb01 <nyamambanyaguch@gmail.com>
2024-02-09 16:31:16 +0300

    models

--- models/base_model.py
+++ models/base_model.py
@@ -2,27 +2,28 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         self.kwargs = kwargs
-    
+
     def __str__(self):
         return f"{self.__class__.__name__} {self.id}"
-    
+
     def save(self):
         self.updated_at = datetime.now()
-        return self.updated_at 
-    
+        return self.updated_at
+
     def to_dict(self):
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
 
         if self.kwargs:
-            for k,v in self.kwargs.items():
+            for k, v in self.kwargs.items():
                 setattr(self, k, v)
         self.created_at = datetime.fromisoformat(self.created_at)
         self.updated_at = datetime.fromisoformat(self.updated_at)
-        return self.__dict__ 
+        return self.__dict__
---
+++ models/engine/file_storage.py
@@ -0,0 +1,15 @@
+import json
+
+class FileStorage:
+    __file_path
+    __objects
+    BaseModel()
+
+
+    def all(self): 
+
+    def new(self, obj): 
+
+    def save(self):
+        
+    def reload(self)
