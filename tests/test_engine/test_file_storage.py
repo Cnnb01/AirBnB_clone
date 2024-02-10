c1253ea2dfc4ecb510a300b95dddec9dfa52a9ca
Cnnb01 <nyamambanyaguch@gmail.com>
2024-02-10 22:23:37 +0300

    tests

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,26 +1,44 @@
-# #!/usr/bin/python3
-# import unittest
-# import json
-# import os
-# from models.base_model import BaseModel
-# from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
+"""Defines unittests for file_storage.py."""
+import unittest
+from models.base_model import BaseModel
+import models
+from models.engine.file_storage import FileStorage
 
-# class TestFileStorage(unittest.TestCase):
 
-#         # def test_all(self):
-       
-#         # def test_new(self) :
+class TestFileStorage(unittest.TestCase):
+    """Unittests for testing instantiation of the FileStorage class."""
 
-#         # def test_save(self):
+    def test_all(self):
+        """tests the all method"""
+        models.storage = FileStorage()
+        all_objects = models.storage.all()
+        self.assertIsInstance(all_objects, dict)
+        self.assertEqual(all_objects, {})
 
-#         def test_reload(self):
-#                 import models
-#                 obj = BaseModel()
-#                 obj.save()
-#                 models.storage.reload()
-#                 loaded_objects = models.storage.all()
-#                 self.assertIn(f"BaseModel.{obj.id}", loaded_objects)
+    def test_new(self):
+        """tests the new method"""
+        models.storage = FileStorage()
+        obj = BaseModel()
+        models.storage.new(obj)
+        all_objects = models.storage.all()
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.assertIn(key, all_objects)
+        self.assertEqual(all_objects[key], obj.to_dict())
 
-# if __name__ == '__main__':
-#     unittest.main()
+    def test_save_reload(self):
+        """tests the save and reload method"""
+        models.storage = FileStorage()
+        obj = BaseModel()
+        models.storage.new(obj)
+        models.storage.save()
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_objects = new_storage.all()
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.assertIn(key, all_objects)
+        self.assertEqual(all_objects[key], obj.to_dict())
 
+
+if __name__ == '__main__':
+    unittest.main()
