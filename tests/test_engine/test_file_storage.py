dd6808648b44261bded603e84849fb5db9c6a5a6
Cnnb01 <nyamambanyaguch@gmail.com>
2024-02-10 18:44:44 +0300

    tests

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,26 @@
+# #!/usr/bin/python3
+# import unittest
+# import json
+# import os
+# from models.base_model import BaseModel
+# from models.engine.file_storage import FileStorage
+
+# class TestFileStorage(unittest.TestCase):
+
+#         # def test_all(self):
+       
+#         # def test_new(self) :
+
+#         # def test_save(self):
+
+#         def test_reload(self):
+#                 import models
+#                 obj = BaseModel()
+#                 obj.save()
+#                 models.storage.reload()
+#                 loaded_objects = models.storage.all()
+#                 self.assertIn(f"BaseModel.{obj.id}", loaded_objects)
+
+# if __name__ == '__main__':
+#     unittest.main()
+
