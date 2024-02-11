599bb5b2d27992df526f87ec41f29041168c3553
Cnnb01 <nyamambanyaguch@gmail.com>
2024-02-11 16:14:50 +0300

    models

--- console.py
+++ console.py
@@ -6,18 +6,19 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """Defining interpreter"""
     prompt = "(hbnb) "
-      
 
     def emptyline(self):
         """Ignore empty spaces"""
         pass
-    
+
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
-    
+
     def do_EOF(self, line):
         """Terminates the loop"""
         return True
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
