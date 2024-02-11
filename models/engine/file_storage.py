64a14328b417f3793b24e48feb5392bee81ced8c
Cnnb01 <nyamambanyaguch@gmail.com>
2024-02-11 20:22:27 +0300

    models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,6 +24,11 @@ import json
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -76,7 +81,7 @@ class FileStorage:
         """
         obj_dict = {}
         for obj in self.__objects.values():
-            obj_dict[obj['__class__'] + '.' + obj['id']] = obj# .to_dict()
+            obj_dict[obj['__class__'] + '.' + obj['id']] = obj
         with open(self.__file_path, "w") as file:
             json.dump(obj_dict, file)
 
